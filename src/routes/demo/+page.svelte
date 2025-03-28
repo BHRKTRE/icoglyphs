11771d9dff2fc611e59b7d4db3eb9100086339af
LM45PixelPsi <leo.bpdr@gmail.com>
2025-03-28 13:59:05 +0100

    core(/icoGlyphButton): Remove icoGlyphButton folder and all components inside, as they are now replaced with new components

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,23 +1,9 @@
 <script>
-	import IcoGlyphButton from '$lib/app/ui/components/icoGlyphButton/IcoGlyphButton.svelte';
 	import icoGlyphs from '$lib/index.js';
-	import icoGlyphButtonPropsConstructor from '$lib/app/ui/components/icoGlyphButton/propsConstructor.js';
 	import appState from '$lib/app/core/stores/appState.svelte.js';
 	import psi from '$lib/app/ui/utils/psi.js';
 	import MorphingPath from '$lib/app/ui/components/MorphingPath.svelte';
 	import Tooltip from '$lib/app/ui/components/Tooltip.svelte';
-
-	let bbb = $state(new icoGlyphButtonPropsConstructor());
-	bbb.add('arrow-left', () => console.log('function 2'), {
-		to: 'arrow-right',
-		tooltip: bbb.tooltip('a faire', 'bottom'),
-		text: 'Prends le bail'
-	});
-	bbb.add('arrow-right', () => console.log('function 1'), {
-		tooltip: bbb.tooltip('coucou', 'top'),
-		text: '.png'
-	});
-	// bbb.add('arrow-top', () => console.log('function 3'));
 
 	const styleyy = {
 		// background: 'var(--b2)',
@@ -38,15 +24,6 @@
 	function changeDuration() {
 		animeDurationButton1 = 2000;
 	}
-
-	/**
-	 *Doc:
-	 * @notice This will trigger when the parent change the name button
-	 * @param {string} selected - Must be a valid icoGlyph name and being in the button	* can be used to bind the selected prop of IcoGlyphButton
-	 * Thus you can change the button state from anywhere in the app
-	 * Ex: if the state button depend of a boolean in the parent component
-	 * bind:selected={...}
-	 */
 
 	// $inspect(appState.modes);
 
@@ -80,12 +57,7 @@
 	<button class="text-button" onclick={changeActualState2}>change2</button>
 
 	<div class="justforspace"></div>
-	<IcoGlyphButton
-		bind:selected={selectedButton1}
-		animeDuration={animeDurationButton1}
-		buttonConfig={bbb}
-		buttonStyle={styleyy}
-	/>
+
 	<br />
 	<br />
 	<button class="text-button" onclick={changeActualState}>change</button>
