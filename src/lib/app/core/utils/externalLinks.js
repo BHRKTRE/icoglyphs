47aa9c5c72ed4bd5093b3f912fd15d4830d34d87
LM45PixelPsi <leo.bpdr@gmail.com>
2025-04-14 14:33:26 +0200

    core(externalLinks): Update github link

--- src/lib/app/core/utils/externalLinks.js
+++ src/lib/app/core/utils/externalLinks.js
@@ -1,6 +1,6 @@
 const externalLinks = {
 	discord: 'https://discord.gg/RZ8ndJEKpF',
-	github: 'https://github.com/LM45PixelPsi/icoglyphs'
+	github: 'https://github.com/BHRKTRE/icoglyphs'
 };
 
 export default externalLinks;
