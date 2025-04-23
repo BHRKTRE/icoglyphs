f2d6cb7e73841c6d67c4681ac21db1fbf69a9b23
LM45PixelPsi <leo.bpdr@gmail.com>
2025-04-23 15:36:07 +0200

    feat(icon/preview): notice TODO and start architecture

--- src/lib/library/external/getSvgData/getSvgAttributes.js
+++ src/lib/library/external/getSvgData/getSvgAttributes.js
@@ -11,6 +11,7 @@ function getSvgAttributes(icoGlyphName) {
 			xmlns: 'http://www.w3.org/2000/svg',
 			viewBox: '-50 -50 100 100',
 			focusable: false, // ?
+			// 'data-icoGlyph': icoGlyphName,
 			role: 'img'
 			// "aria-label": ariaLabel,
 		};
---
+++ src/routes/icon/[icoGlyphNameSlug]/preview/+server.js
@@ -0,0 +1,9 @@
+/**
+ * Automatically generates a PNG for metadata preview.
+ *
+ * /icons/icon-name/preview/icon-name.jpg
+ *
+ * TODO:
+ * |> Render png on this page
+ * |> Consider using sharp
+ */
