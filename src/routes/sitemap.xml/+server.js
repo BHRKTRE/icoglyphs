fda3c9171ed3837c9660ec50847973b54a87bb71
LM45PixelPsi <leo.bpdr@gmail.com>
2025-05-23 16:18:47 +0200

    refactor(sitemap.xml)

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,55 +1,43 @@
-import { getDefaultHomepageIconsWithAliases } from '$lib/app/utils/homePageData.svelte.js';
-
-/**
- * 	xmlns:xhtml="https://www.w3.org/1999/xhtml"
-			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
- * 
- */
+import icoGlyphs from '$lib/index.js';
 
 const mainUrl = 'https://icoglyphs.com/';
+const lastModDate = '2025-04-25';
 
-const generateSitemap = () => {
-	const DefaultHomepageIcons = getDefaultHomepageIconsWithAliases();
-	let sitemapXml = '';
-
-	DefaultHomepageIcons.forEach((iconAlias) => {
-		sitemapXml += `
-        <url>
-            <loc>${mainUrl}icon/${iconAlias}</loc>
-            <lastmod>2025-04-25</lastmod>
-            <changefreq>monthly</changefreq>
-            <priority>0.5</priority>
-        </url>\n`;
-	});
-
-	return sitemapXml;
-};
+const homePageIcons = () =>
+	icoGlyphs.library
+		.filter((ig) => ig.is_public)
+		.map((ig) => ig.aliases[0])
+		.reverse()
+		.map(
+			(alias) => `
+    <url>
+      <loc>${mainUrl}icon/${alias}</loc>
+      <lastmod>${lastModDate}</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>0.5</priority>
+    </url>`
+		)
+		.join('\n');
 
 export async function GET() {
 	return new Response(
 		`
-		<?xml version="1.0" encoding="UTF-8" ?>
-		<urlset
-			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-		>
-			
-        <url>
-            <loc>${mainUrl}</loc>
-            <lastmod>2025-04-25</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>1.0</priority>
-        </url>
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
 
-        ${generateSitemap()}
+      <url>
+        <loc>${mainUrl}</loc>
+        <lastmod>${lastModDate}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+      </url>
 
-		</urlset>`.trim(),
+      ${homePageIcons()}
+
+    </urlset>
+    `.trim(),
 		{
-			headers: {
-				'Content-Type': 'application/xml'
-			}
+			headers: { 'Content-Type': 'application/xml' }
 		}
 	);
 }
