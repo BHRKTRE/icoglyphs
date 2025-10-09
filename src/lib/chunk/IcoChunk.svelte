e11ff4f640460f2719c39986121e5db95241f910
LM45PixelPsi <leo.bpdr@gmail.com>
2025-10-09 16:01:56 +0200

    feat(icoChunk): add setSegmentToLine()

--- src/lib/chunk/IcoChunk.svelte
+++ src/lib/chunk/IcoChunk.svelte
@@ -5,7 +5,7 @@
 
 	let M = $state({ x: 0, y: -35 });
 	let curves = $state([
-		{ x1: 6.6667, y1: -27.6667, x2: 13.3333, y2: -20.3333, x: 20, y: -13 },
+		{ x1: 13, y1: -2, x2: 13.3333, y2: -20.3333, x: 20, y: -13 },
 		{ x1: 15.3333, y1: -3.3333, x2: 10.6667, y2: 6.3333, x: 6, y: 16 },
 		{ x1: -6, y1: 9, x2: -18, y2: 2, x: -30, y: -5 },
 		{ x1: -25.3333, y1: -12, x2: -20.6667, y2: -19, x: -16, y: -26 }
@@ -19,22 +19,38 @@
 	);
 
 	const alignRemainingPoints = () => {
-		const segmentEnd = curves[segmentsNumber - 1]; // point de référence
+		// Aligns all remaining curve segments (after the current segmentNumber)
+		// to the end point of the last defined segment.
+
+		const lastSegment = curves[segmentsNumber - 1];
 		const len = curves.length;
 
 		for (let i = segmentsNumber; i < len; i++) {
-			const currentPoint = curves[i]; // point courant
-			currentPoint.x1 = segmentEnd.x;
-			currentPoint.y1 = segmentEnd.y;
-			currentPoint.x2 = segmentEnd.x;
-			currentPoint.y2 = segmentEnd.y;
-			currentPoint.x = segmentEnd.x;
-			currentPoint.y = segmentEnd.y;
+			const currentPoint = curves[i];
+			currentPoint.x1 = lastSegment.x;
+			currentPoint.y1 = lastSegment.y;
+			currentPoint.x2 = lastSegment.x;
+			currentPoint.y2 = lastSegment.y;
+			currentPoint.x = lastSegment.x;
+			currentPoint.y = lastSegment.y;
+		}
+	};
+
+	const setSegmentToLine = () => {
+		// ex
+		const segment = curves[0];
+
+		if (segmentType === 'line') {
+			segment.x1 = segment.x;
+			segment.y1 = segment.y;
+			segment.x2 = segment.x;
+			segment.y2 = segment.y;
 		}
 	};
 
 	const modPath = () => {
 		alignRemainingPoints();
+		setSegmentToLine();
 	};
 
 	// $inspect(curves[0]);
